469e613efd6fd8f2d819246ce01474a7b37b6042
Aman <amanahuja@gmail.com>
2010-12-11 20:53:32 -0500

    Updated Models and initialized app meta

--- meta/models.py
+++ meta/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+class Bestseller(models.Model):
+    save_date = models.DateTimeField('date saved', default=datetime.datetime.now())
+    sentence = models.CharField(max_length=400)
+    votes = models.IntegerField(default=1)
+
+    def __unicode__(self):
+        return str(self.sentence)
+
--- settings.py
+++ settings.py
@@ -102,4 +102,5 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'pablo.sentence',
+    'pablo.meta',
 )
